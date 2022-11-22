30d7d655d90b8d26fbc77de74871721fb786e413
bakostamas1 <bakostamas@indamail.hu>
2022-11-22 17:30:27 +0100

    creating some flyway migrations

---
+++ MovieDb/src/main/resources/db/migration/V2_AddingActorTable.sql
@@ -0,0 +1,6 @@
+CREATE TABLE actor (
+                       id    bigserial primary key,
+                       name  TEXT NOT NULL,
+                       movie bigint REFERENCES movie (id),
+                       unique (name, movie)
+);
