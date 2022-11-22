0fecc76d45acb7f456ad92cf86d62a237bf42d29
bakostamas1 <bakostamas@indamail.hu>
2022-11-22 17:27:00 +0100

    creating movieservice with some data manipulation logic

---
+++ MovieDb/src/main/java/com/tamasbakos/moviedb/services/MovieService.java
@@ -0,0 +1,46 @@
+package com.tamasbakos.moviedb.services;
+
+import com.tamasbakos.moviedb.customexceptions.NotFoundException;
+import com.tamasbakos.moviedb.models.Movie;
+import com.tamasbakos.moviedb.repositories.MovieDao;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MovieService {
+  private final MovieDao movieDao;
+
+  public MovieService(MovieDao movieDao) {
+    this.movieDao = movieDao;
+  }
+
+  public List<Movie> getMovies() {
+    return movieDao.selectMovies();
+  }
+
+  public void addNewMovie(Movie movie) {
+    // TODO: check if movie exists
+    int result = movieDao.insertMovie(movie);
+    if (result != 1) {
+      throw new IllegalStateException("oops something went wrong");
+    }
+  }
+
+  public void deleteMovie(Integer id) {
+    Optional<Movie> movies = movieDao.selectMovieById(id);
+    movies.ifPresentOrElse(movie -> {
+      int result = movieDao.deleteMovie(id);
+      if (result != 1) {
+        throw new IllegalStateException("oops could not delete movie");
+      }
+    }, () -> {
+      throw new NotFoundException(String.format("Movie with id %s not found", id));
+    });
+  }
+
+  public Movie getMovie(int id) {
+    return movieDao.selectMovieById(id)
+        .orElseThrow(() -> new NotFoundException(String.format("Movie with id %s not found", id)));
+  }
+}
