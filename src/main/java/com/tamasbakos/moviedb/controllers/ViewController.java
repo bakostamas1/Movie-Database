b9c6ef0907b9434b597bb676361408788763ef24
bakostamas1 <bakostamas@indamail.hu>
2022-11-22 20:00:48 +0100

    added view endpoint, creating html pages

--- src/main/java/com/tamasbakos/moviedb/controllers/ViewController.java
+++ src/main/java/com/tamasbakos/moviedb/controllers/ViewController.java
@@ -1,16 +1,27 @@
 package com.tamasbakos.moviedb.controllers;
 
+import com.tamasbakos.moviedb.services.MovieService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ViewController {
 
-  public ViewController() {
+  private final MovieService movieService;
+
+  public ViewController(MovieService movieService) {
+    this.movieService = movieService;
   }
 
   @GetMapping()
-  public String homepage() {
+  public String homepage(Model model) {
     return "homepage";
+  }
+
+  @GetMapping("movies")
+  public String allMovies(Model model) {
+    model.addAttribute("allMovies", movieService.getAllMovies());
+    return "currentMovieList";
   }
 }
